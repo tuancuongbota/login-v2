93fb0031e7e05a94db4ac5c0d35ee95c89885daa
tuancuongbota <93958224+tuancuongbota@users.noreply.github.com>
2023-09-26 17:08:00 +0700

    update login

---
+++ statics/js/login.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    if ($(".forgot-pos-password").length) {
+        $('.forgot-pos-password').click(function () {
+            $('.login-form-view').hide();
+            $('.forgot-pass-view').show();
+        });
+    }
+    if ($("#back-btn").length) {
+        $('#back-btn').click(function () {
+            $('.login-form-view').show();
+            $('.forgot-pass-view').hide();
+        });
+    }
+});
