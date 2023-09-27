85ee6b5227b183a44800187d30144b87dce77c57
tuancuongbota <93958224+tuancuongbota@users.noreply.github.com>
2023-09-27 08:34:53 +0700

    update

--- statics/js/login.js
+++ statics/js/login.js
@@ -1,4 +1,38 @@
 $(document).ready(function () {
+    $("#login-form").validate({
+        rules: {
+            email: {
+                required: true,
+                email: true 
+            },
+            password: {
+                required: true
+            }
+        },
+        messages: {
+            email: {
+                required: "Email không được để trống.",
+                email: "Email không đúng định dạng."
+            },
+            password: {
+                required: "Mật khẩu không được để trống."
+            }
+        }
+    });
+    $("#forget-form").validate({
+        rules: {
+            email: {
+                required: true,
+                email: true 
+            },
+        },
+        messages: {
+            email: {
+                required: "Email không được để trống.",
+                email: "Email không đúng định dạng."
+            },
+        }
+    });
     if ($(".forgot-pos-password").length) {
         $('.forgot-pos-password').click(function () {
             $('.login-form-view').hide();
